2d45da36341c05bf402dea5fd3353e360d76e143
abdulbasit <ba3568008@gmail.com>
2023-10-16 17:13:09 +0500

    resolved error of vercel

--- src/components/shared/Messages.tsx
+++ src/components/shared/Messages.tsx
@@ -12,8 +12,10 @@ const Messages = ({ initialMessages, sessionId, sessionImg, chatPartner, chatId 
 
 
     useEffect(() => {
+        console.log('Before pusher on message like start')
         pusherClient.subscribe(toPusherKey(`chat:${chatId}`));
-
+        console.log('subscribe to chat: ' , `chat:${chatId}`)
+        
         const messageHandler = (message:message) => {
             setMessages((prev) => [message, ...prev]);
         }
