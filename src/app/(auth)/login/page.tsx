74e230e76ab633e55d010776cb82dbb2665c1a7b
abdulbasit <ba3568008@gmail.com>
2024-07-22 17:15:32 +0500

    Changed logo

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Button from "@/components/shared/ui/Button"
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 import { useState } from "react"
 import { toast } from "react-hot-toast";
 
@@ -22,7 +23,7 @@ const Login = () => {
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className="w-full flex flex-col items-centermax-w-md space-y-8">
                 <div className="flex flex-col items-center gap-8">
-                    logo
+                    <Image width={160} height={160} alt="WalkteeChatAppLogo" src={"/WalkTeeLogo.png"} />
                     <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-400">Sign into your account</h2>
                 </div>
                 <Button isLoading={isLoading} type="button" className="max-w-sm mx-auto w-full " onClick={loginWithGoogle}>
